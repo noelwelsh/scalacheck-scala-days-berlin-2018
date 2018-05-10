6b15ae5eb094c0b578b43f3d5035a3bb3c01ef26
Noel Welsh <noel@untyped.com>
2018-05-10 18:04:18 +0100

    WIP on ScalaCheck scaffolding

---
+++ src/main/tut/scalacheck/scaffolding.scala
@@ -0,0 +1,5 @@
+## ScalaCheck Test Scaffolding
+
+We've almost finished our initial tour of ScalaCheck. All that remains is to see the scaffolding that ScalaCheck provides to define test cases that we can run from our build tool.
+
+There are two concepts here, that unfortunately share the same name: the `Properties` class, which declares a collection of tests, and the `property` method in `Properties`, which...
