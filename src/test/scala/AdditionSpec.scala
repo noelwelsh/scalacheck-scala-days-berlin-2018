73ea2afc7cca611c2710908b3945dabc3bead8dc
Noel Welsh <noel@untyped.com>
2018-05-10 16:48:18 +0100

    Spec for the addition example in the text

---
+++ src/test/scala/AdditionSpec.scala
@@ -0,0 +1,16 @@
+import org.scalacheck._
+import org.scalacheck.Prop.forAll
+
+object AdditionSpecification extends Properties("Addition") {
+  property("identity") = forAll { (x: Int) => x + 0 == 0 }
+
+  property("associativity") = forAll { (x: Int, y: Int, z: Int) =>
+    (x + y) + z == x + (y + z)
+  }
+
+  property("commutativity") = forAll { (x: Int, y: Int) =>
+    x + y == y + x
+  }
+
+  property("invertibility") = forAll { (x: Int) => x + 0 == 0 }
+}
